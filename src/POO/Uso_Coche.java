a7a4ce4ef3ff6567a25c3221bc68d304a7a57fd7
Arurth <arthurc.alvarez@gmail.com>
2023-08-12 16:28:36 -0300

    Subiendo la clase Uso_coche

---
+++ src/POO/Uso_Coche.java
@@ -0,0 +1,33 @@
+package POO;
+
+import javax.print.attribute.standard.JobMessageFromOperator;
+import javax.swing.*;
+
+public class Uso_Coche {
+
+    public static void main(String[] args) {
+
+        Coche micoche = new Coche();
+
+        micoche.establece_color(JOptionPane.showInputDialog("Introduce un color"));
+
+        System.out.println(micoche.dime_datos_generales());
+
+        System.out.println(micoche.dime_color());
+        
+        micoche.configura_asientos(JOptionPane.showInputDialog("¿Tiene asientos de cuero?"));
+
+        System.out.println(micoche.dime_asientos());
+
+        micoche.configura_climatizador(JOptionPane.showInputDialog("¿Tiene climatizador?"));
+
+        System.out.println(micoche.dime_climatizador());
+
+        System.out.println(micoche.dime_peso_coche());
+
+        System.out.println("El precio final del coche es: " + micoche.precio_coche());
+
+
+    }
+
+}
